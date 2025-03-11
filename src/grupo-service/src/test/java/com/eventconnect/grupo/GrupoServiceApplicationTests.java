2e71940a480e7f870c49e50cd855c9dbd30d0d5f
dmmo2003 <dmoralesmarotogmail.com>
2025-03-11 16:16:55 +0100

    Creacion rama

---
+++ src/grupo-service/src/test/java/com/eventconnect/grupo/GrupoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventconnect.grupo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
