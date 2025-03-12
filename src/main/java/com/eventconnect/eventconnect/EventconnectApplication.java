a805adebdd1d033d75ef71f6d99c5039b019da9e
Hast11 <andres2000gil@gmail.com>
2025-03-12 15:41:23 +0100

    Esqueleto SpringBoot

---
+++ src/main/java/com/eventconnect/eventconnect/EventconnectApplication.java
@@ -0,0 +1,13 @@
+package com.eventconnect.eventconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventconnectApplication.class, args);
+	}
+
+}
