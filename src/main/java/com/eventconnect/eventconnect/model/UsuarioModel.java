762499f621d8f08bef8621e6140a22dedcc2be09
davidsancho09 <99362429+davidsancho09@users.noreply.github.com>
2025-03-12 20:19:18 +0100

    Añadir imports listas a UsuarioModel.java

--- src/main/java/com/eventconnect/eventconnect/model/UsuarioModel.java
+++ src/main/java/com/eventconnect/eventconnect/model/UsuarioModel.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "usuarios") // Nombre de la tabla en la BD
