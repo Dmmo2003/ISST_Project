01e54cc72f64f3863e71180b13eeebb23d5a56c0
itsPablo2003 <pdunamuno@gmail.com>
2025-03-15 15:16:08 +0100

    Controller hecho

--- src/main/java/controllers/mensajeController.java
+++ src/main/java/controllers/mensajeController.java
@@ -1,6 +1,41 @@
 package controllers;
 
-public class mensajeController {
+import models.MensajeModel;
+import services.MensajeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/mensajes")
+public class MensajeController {
+
+    @Autowired
+    private MensajeService mensajeService;
+
+    @GetMapping
+    public List<MensajeModel> obtenerTodos() {
+        return mensajeService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public MensajeModel obtenerPorId(@PathVariable Long id) {
+        return mensajeService.obtenerPorId(id);
+    }
+
+    @PostMapping
+    public MensajeModel crearMensaje(@RequestBody MensajeModel mensaje) {
+        return mensajeService.crearMensaje(mensaje);
+    }
+
+    @PutMapping("/{id}")
+    public MensajeModel actualizarMensaje(@PathVariable Long id, @RequestBody MensajeModel mensaje) {
+        return mensajeService.actualizarMensaje(id, mensaje);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarMensaje(@PathVariable Long id) {
+        mensajeService.eliminarMensaje(id);
+    }
 }
-
