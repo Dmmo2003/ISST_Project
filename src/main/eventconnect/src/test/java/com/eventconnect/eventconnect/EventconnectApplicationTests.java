1a595f53f4f647d8070b93c06901549e29dcf95a
itsPablo2003 <pdunamuno@gmail.com>
2025-03-31 11:31:02 +0200

    crear Springboot

---
+++ src/main/eventconnect/src/test/java/com/eventconnect/eventconnect/EventconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventconnect.eventconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
