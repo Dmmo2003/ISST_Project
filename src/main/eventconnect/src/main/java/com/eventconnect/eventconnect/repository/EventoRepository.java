ad4be0c845669e06b3e0cf323d0a5f15de2e696e
itsPablo2003 <pdunamuno@gmail.com>
2025-03-31 16:27:06 +0200

    MainController y EventoRepository

--- src/main/eventconnect/src/main/java/com/eventconnect/eventconnect/repository/EventoRepository.java
+++ src/main/eventconnect/src/main/java/com/eventconnect/eventconnect/repository/EventoRepository.java
@@ -1,8 +1,17 @@
 package com.eventconnect.eventconnect.repository;
 
 import com.eventconnect.eventconnect.model.Evento;
+import com.eventconnect.eventconnect.model.Grupo;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventoRepository extends JpaRepository<Evento, Integer> {
-    // Aquí puedes agregar consultas personalizadas si es necesario
+    
+    List<Evento> findByAdminId(int adminId);
+    
+    List<Evento> findByOrganizadorId(int organizadorId);
+    
+
 }
